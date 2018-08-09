3168ef3d2a44fda18bfb5aa5c23e5ba7b9e7aaa7
sy <somarl@live.com>
2018-08-09 13:35:39 +0800

    feat(monad): add example mappable

--- src/impl/monad.ts
+++ src/impl/monad.ts
@@ -1,3 +1,21 @@
+import { curry, compose } from 'src/porter'
+
+interface IMappable<T> {
+    __value__: T | F<T>
+    map <R> (f: F1<T, R>): IMappable<R> | IO<R>
+}
+
+
+/**
+ * @internal
+ * fmap :: Functor f => (a -> b) -> f a -> f b
+ */
+export const fmap = curry(<T, R>(f: F1<T, R>, mappable: IMappable<T>) => mappable.map(f)) as /** @interface */ {
+    <T, R>(f: F1<T, R>): (mappable: IMappable<T>) => IMappable<R>
+    <T, R>(f: F1<T, R>, mappable: IMappable<T>): IMappable<R>
+}
+
+
 /**
  * @monad
  */
@@ -11,3 +29,22 @@ export class Monad {
 }
 
 // TODO: resolvable
+
+
+/**
+ * @monad
+ */
+export class IO<T = any> implements IMappable<T> {
+    public constructor (
+        public __value__: F0<T> // tslint:disable-line
+    ) {
+    }
+
+    public static of <T> (v: T) {
+        return new IO(() => v)
+    }
+
+    public map <R> (f: F1<T, R>) {
+        return new IO(compose(f, this.__value__))
+    }
+}
