422835cdcda4e0159a2917f5e3589df66b850c07
sy <somarl@live.com>
2018-08-22 14:02:44 +0800

    feat(array); add trial unary

--- src/impl/array.ts
+++ src/impl/array.ts
@@ -120,3 +120,12 @@ export const map = named('map')(curry((f: F, x: any) => {
 // TODO: transduce
 
 // TODO: take
+
+
+/**
+ * @deprecated maybe uncessary in typescript
+ * @sig unary :: f -> f
+ */
+// export function unary <T, R> (f: F1<T, R> | F2<T, A, R>| F3<T, A, A, R> | F<R>) {
+//     return named(f.name)((arg: T) => f(arg))
+// }
